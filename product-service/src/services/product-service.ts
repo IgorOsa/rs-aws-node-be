74b03a0c436b300e8e22c46a565379daf902f03d
Ihor Osadchyi <igorosa.com@gmail.com>
2022-06-26 16:38:21 +0300

    feat: connect product service to db data

--- product-service/src/services/product-service.ts
+++ product-service/src/services/product-service.ts
@@ -1,47 +1,32 @@
+import dbClient from "./../db-client";
 import { ProductResource } from "../types/api-types";
+import { QueryConfig } from "pg";
 
-const products: ProductResource[] = [
-    {
-        id: 'prod0001',
-        title: 'Windows 10 Home',
-        description: 'License for Windows 10 Home',
-        price: 149.99
-    },
-    {
-        id: 'prod0002',
-        title: 'Windows 10 Professional',
-        description: 'License for Windows 10 Professional',
-        price: 199.99
-    },
-    {
-        id: 'prod0003',
-        title: 'Windows 11 Home',
-        description: 'License for Windows 11 Home',
-        price: 199.99
-    },
-    {
-        id: 'prod0004',
-        title: 'Windows 11 Professional',
-        description: 'License for Windows 11 Professional',
-        price: 249.99
-    },
-    {
-        id: 'prod0005',
-        title: 'MS Office 2022 Home',
-        description: 'Lifetime License for MS Office 2022 Home',
-        price: 149.99
-    },
-    {
-        id: 'prod0006',
-        title: 'MS Office 2022 Professional',
-        description: 'Lifetime License for MS Office 2022 Home',
-        price: 249.99
-    }
-];
+
+const tableName = 'products';
+const subTableName = 'stocks';
 
 export default {
-    getAll: async () => products,
-    getById: async (id: string) => products.find(el => el.id === id)
+    getAll: async () => {
+        const query = {
+            text: `SELECT id, count, price, title, description FROM ${tableName} 
+            LEFT JOIN ${subTableName} on ${tableName}.id=${subTableName}.product_id`,
+        } as QueryConfig;
+
+        const result = await dbClient.query(query);
+        return result.rows ? result.rows : null;
+    },
+    getById: async (id: string) => {
+        const query = {
+            text: `SELECT id, count, price, title, description FROM ${tableName} 
+            LEFT JOIN ${subTableName} on ${tableName}.id=${subTableName}.product_id 
+            WHERE id = $1`,
+            values: [id],
+        } as QueryConfig;
+
+        const result = await dbClient.query(query);
+        return result.rows[0] ? result.rows[0] : null;
+    }
 }
 
 export interface ProductService {
