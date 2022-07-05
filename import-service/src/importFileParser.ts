b81817703165761c1349361f3dee012313130b54
Ihor Osadchyi <igorosa.com@gmail.com>
2022-07-06 00:03:35 +0300

    fix: replace forEach with forOf to work in all cases

--- import-service/src/importFileParser.ts
+++ import-service/src/importFileParser.ts
@@ -1,6 +1,6 @@
 import csv from 'csv-parser';
 import { Readable } from 'stream';
-import { S3Client, GetObjectCommand, CopyObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, GetObjectCommand, CopyObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 const BUCKET = process.env.BUCKET;
 const region = process.env.REGION;
@@ -8,30 +8,51 @@ const region = process.env.REGION;
 const s3 = new S3Client({ region });
 
 export const importFileParser = async (event) => {
-    event.Records.forEach(async (record) => {
+    console.log(event);
+
+    for (let record of event.Records) {
+        console.log(record);
+
+        const Bucket = record.s3.bucket.name;
+        const Key = decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '));
+
         const response = await s3.send(new GetObjectCommand({
-            Bucket: BUCKET,
-            Key: record.s3.object.key
+            Bucket,
+            Key
         }));
 
         const s3Stream = response.Body as Readable;
 
-        s3Stream.pipe(csv())
-            .on('data', (data) => {
-                console.log(data);
+        const s3StreamProcessor = (stream) => {
+            return new Promise((resolve, reject) => {
+                stream
+                    .pipe(csv())
+                    .on('error', (err) => reject(err))
+                    .on('data', (data) => {
+                        console.log(data);
+                    })
+                    .on('end', async () => {
+                        console.log(`Copy from ${Bucket}/${Key}`);
+
+                        await s3.send(new CopyObjectCommand({
+                            Bucket,
+                            CopySource: `${Bucket}/${Key}`,
+                            Key: Key.replace('uploaded', 'parsed')
+                        })).catch((err) => console.log(err));
+
+                        console.log(`Moved to ${Bucket}/${Key.replace('uploaded', 'parsed')}`);
+
+                        await s3.send(new DeleteObjectCommand({
+                            Bucket,
+                            Key: record.s3.object.key
+                        })).catch((err) => console.log(err));
+
+                    });
             })
-            .on('end', async () => {
-                console.log(`Copy from ${BUCKET}/${record.s3.object.key}`);
+        }
 
-                await s3.send(new CopyObjectCommand({
-                    Bucket: BUCKET,
-                    CopySource: `${BUCKET}/${record.s3.object.key}`,
-                    Key: record.s3.object.key.replace('uploaded', 'parsed')
-                }));
-
-                console.log(`Copied into ${BUCKET}/${record.s3.object.key.replace('uploaded', 'parsed')}`);
-            });
-    });
+        await s3StreamProcessor(s3Stream);
+    }
 }
 
 export default importFileParser;
