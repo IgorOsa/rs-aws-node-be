fa01ecf20887ae88aba4718761b40658b4e7091c
Ihor Osadchyi <igorosa.com@gmail.com>
2022-07-11 10:58:56 +0300

    feat: Add send messages to SNS topic

--- product-service/src/functions/catalogBatchProcess.ts
+++ product-service/src/functions/catalogBatchProcess.ts
@@ -1,4 +1,5 @@
 import { SQSClient, DeleteMessageCommand } from "@aws-sdk/client-sqs";
+import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
 import schema from "../schemas/createProductValidation";
 import productService from '../services/product-service';
 import { errorResponse, successResponse } from "../utils/responseHandler";
@@ -6,11 +7,14 @@ import { errorResponse, successResponse } from "../utils/responseHandler";
 
 const region = process.env.REGION || 'eu-west-1';
 const QueueUrl = process.env.SQS_URL;
+const SNS_ARN = process.env.SNS_ARN;
 
 const sqs = new SQSClient({ region });
+const sns = new SNSClient({ region });
 
 export const catalogBatchProcess = async (event) => {
     console.log(event.Records);
+    const addedProducts = [];
 
     for (const message of event.Records) {
         console.log(message);
@@ -41,12 +45,29 @@ export const catalogBatchProcess = async (event) => {
 
             const result = await productService.create(product);
 
-            return successResponse(result, 201);
+            addedProducts.push(product);
+
+            console.log('Product creation result:', result);
         } catch (error) {
             console.log(error);
             return errorResponse(error, 400);
         }
     }
+
+    try {
+        const result = await sns.send(new PublishCommand({
+            Subject: 'New product(s) added to DB',
+            Message: JSON.stringify(addedProducts),
+            TopicArn: SNS_ARN,
+            // TargetArn: SNS_ARN,
+        }))
+
+        console.log('Message to SNS successfully sent', result);
+    } catch (error) {
+        console.log('Error sending message to SNS', error.message);
+    }
+
+    return successResponse('Catalog Batch Process Successful', 201);
 }
 
 export default catalogBatchProcess;
