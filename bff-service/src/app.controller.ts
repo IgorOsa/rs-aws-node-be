fa2e34deb0cd3ad3d5a8d3499ef9aaba31d194ef
Ihor Osadchyi <igorosa.com@gmail.com>
2022-08-14 22:20:51 +0300

    feat: Add request processing

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Controller, Get, Logger, Post, Req, Res } from '@nestjs/common';
+import axios from 'axios';
+import { Request, Response } from 'express';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/*')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  getHello(@Req() req: Request, @Res() res: Response): any {
+    Logger.debug('originalUrl', req.originalUrl);
+    const recipientServiceName = req.originalUrl.split(/[\/?]/)[1];
+    Logger.debug('recipientServiceName', recipientServiceName);
+    const recipientURL = process.env[recipientServiceName];
+    Logger.debug('recipientURL', recipientURL);
+
+    if (recipientURL) {
+      const axiosConfig = {
+        method: req.method,
+        url: `${recipientURL}${req.originalUrl}`,
+        ...(Object.keys(req.body || {}).length > 0 && { data: req.body }),
+      };
+
+      Logger.debug('axiosConfig', axiosConfig);
+
+      axios(axiosConfig)
+        .then((response) => {
+          res.send(response?.data);
+        })
+        .catch((err) => {
+          Logger.error('axios error', err);
+
+          if (err.response) {
+            const { status, data } = err.response;
+
+            res.sendStatus(status).json(data);
+          } else {
+            res.sendStatus(500).json({ message: err.message });
+          }
+        });
+    } else {
+      res.sendStatus(502).json({ error: 'Cannot process request' });
+    }
   }
 }
